271fa1b3b306494fdbb561197d4ecaec16347761
Taissa Costa <taissa@localhost.localdomain>
2025-06-27 14:56:21 -0300

    cod em java

---
+++ testes/CNPJ1.java
@@ -0,0 +1,11 @@
+public class Test {
+   public void CNPJ() {
+      int cnpj = 1231100011;
+      System.out.println("CNPJ is : " + cnpj);
+   }
+
+   public static void main(String args[]) {
+      Test test = new Test();
+      test.CNPJ();
+   }
+}
